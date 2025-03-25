1069732d96275232301d41527c91f83906075045
Avanti <kantumuchuavanthi@gmail.com>
2025-03-25 12:17:33 -0500

    updated port and removed previous workflow file

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.get('/api/students', (req, res) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
